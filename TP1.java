c2a29f03cb9520d113053980b67d17e203b183a2
SaaDebora <127162776+SaaDebora@users.noreply.github.com>
2023-03-07 00:05:31 -0300

    Cargar ejercicios

---
+++ TP1.java
@@ -0,0 +1,27 @@
+package TP1;
+
+public class TP1 {
+
+	public static void main(String[] args) {
+		//EJERCICION1A
+          int numeroInicio =5;
+          int numeroFin =14; 
+				
+          while(numeroInicio <= numeroFin) {
+        	  
+        	  System.out.println(numeroInicio);
+        	  numeroInicio++;
+          }
+        	  
+          
+		      //EJERCICION1B
+		       numeroInicio =5;
+			while(numeroInicio <= numeroFin) {
+				if(numeroInicio %2 == 0) {
+					System.out.println(numeroInicio);
+				}
+			numeroInicio++;
+			}
+			}
+
+}
